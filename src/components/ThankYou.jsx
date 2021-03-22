57f43b8d5b1b2a8a4250ad790eb4df5410afb279
CapShank <shankiitkgp@gmail.com>
2021-03-23 01:45:01 +0530

    Added my project

---
+++ src/components/ThankYou.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ThankYou() {
+    return (
+        <div>
+            <h1>Thank You for registering with us!</h1>
+        </div>
+    );
+}
+
+export default ThankYou;
