67df55d0216514ca607f12b1ff2ef853c6b4ab9d
CapShank <shankiitkgp@gmail.com>
2021-03-23 10:53:24 +0530

    Updates

---
+++ precache-manifest.af44fcf7012c552af5696f0fbd3bb064.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab07444a505b4b26abe890c14f2b23f",
+    "url": "/Registration/index.html"
+  },
+  {
+    "revision": "76b383f29528a72a6776",
+    "url": "/Registration/static/js/2.8ee657d9.chunk.js"
+  },
+  {
+    "revision": "89dd76776393675be249",
+    "url": "/Registration/static/js/main.f049d384.chunk.js"
+  },
+  {
+    "revision": "4a69756bb3f4c114171a",
+    "url": "/Registration/static/js/runtime-main.74bf197d.js"
+  },
+  {
+    "revision": "336023ccb94ef77b724f5df7c3ac5ce3",
+    "url": "/Registration/static/media/checkbox.336023cc.svg"
+  },
+  {
+    "revision": "cecb19a6abb89857ecf5e3a32be94492",
+    "url": "/Registration/static/media/emptybox.cecb19a6.svg"
+  },
+  {
+    "revision": "2b358eba1af6b72a46e040fe39acc189",
+    "url": "/Registration/static/media/eye.2b358eba.svg"
+  },
+  {
+    "revision": "04d38908336b2155705fd5c569ba69ed",
+    "url": "/Registration/static/media/eyeslash.04d38908.svg"
+  },
+  {
+    "revision": "91dcb12852b91a5764629caff66e6e2e",
+    "url": "/Registration/static/media/tick.91dcb128.svg"
+  }
+]);
